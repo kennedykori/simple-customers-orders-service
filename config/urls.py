3c4b8bd0fe229f947f27633b727fd787c5657987
Kennedy Kori <kennedykori47@gmail.com>
2021-03-03 05:02:06 +0300

    feat: Add swagger and redoc endpoints

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.staticfiles.urls import static
 from django.urls import include, path
 
-from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 # URLs
@@ -36,7 +38,18 @@ urlpatterns = [
     ),
     path('api/', include('apps.core.urls')),
     path('api/', include('apps.shop.urls')),
-    path('api/auth-token/', obtain_auth_token)
+    path('api/auth-token/', ObtainAuthToken.as_view(schema=None)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'api/schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
+    path(
+        'api/schema/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc'
+    ),
 ]
 
 if settings.DEBUG:
