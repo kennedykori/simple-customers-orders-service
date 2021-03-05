bccb2a119ffabaf761e3b4432c2c690dffc44c93
Kennedy Kori <kennedykori47@gmail.com>
2021-03-05 03:14:52 +0300

    chore: Add initial tests for "OrderViewSet" class
    The tests are for the following actions:
    1. add_item
    2. list
    3. update

--- apps/shop/tests/test_apiviews.py
+++ apps/shop/tests/test_apiviews.py
@@ -1,4 +1,5 @@
-from typing import List
+from decimal import Decimal
+from typing import Any, Dict, List
 
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -13,10 +14,19 @@ from rest_framework.test import (
 from ...core.tests.factories import AdminFactory, UserFactory
 
 from ..models import Customer, Employee, Inventory, Order
-from ..serializers import InventorySerializer, LimitedInventorySerializer
+from ..serializers import (
+    InventorySerializer,
+    LimitedInventorySerializer,
+    OrderSerializer
+)
 from ..apiviews import InventoryViewSet
 
-from .factories import CustomerFactory, EmployeeFactory, InventoryFactory
+from .factories import (
+    CustomerFactory,
+    EmployeeFactory,
+    InventoryFactory,
+    OrderFactory
+)
 
 
 # Constant
@@ -255,7 +265,7 @@ class InventoryViewSetTests(APITestCase):
     Tests for the **InventoryViewSet** class.
     """
 
-    def test_list_inventory(self) -> None:
+    def test_list_inventories(self) -> None:
         """
         Ensure that the **InventoryViewSet.list** action works as expected.
         """
@@ -299,7 +309,7 @@ class InventoryViewSetTests(APITestCase):
 
     def test_update_inventory(self) -> None:
         """
-        Ensure that the **InventoryViewSet.list** action works as expected.
+        Ensure that the **InventoryViewSet.update** action works as expected.
         """
         # Test data
         admin: User = AdminFactory.create()
@@ -324,3 +334,237 @@ class InventoryViewSetTests(APITestCase):
         inventory.refresh_from_db()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(inventory.beverage_name, data['beverage_name'])
+
+
+class OrderViewSetTests(APITestCase):
+    """
+    Tests for the **OrderViewSet** class.
+    """
+
+    def test_add_item(self) -> None:
+        """
+        Ensure that the **OrderViewSet.add_item** action works as expected.
+        """
+        # Test data
+        admin: User = AdminFactory.create()
+        customer: Customer = CustomerFactory.create()
+        customer2: Customer = CustomerFactory.create()
+        inventories: List[Inventory] = InventoryFactory.create_batch(
+            3,
+            price=Decimal(10.00)
+        )
+        order = OrderFactory.create(customer=customer)
+        order1 = OrderFactory.create(customer=customer, approved=True)
+        order2 = OrderFactory.create(customer=customer, canceled=True)
+        order3 = OrderFactory.create(customer=customer, pending=True)
+        order4 = OrderFactory.create(customer=customer, rejected=True)
+
+        # Request data
+        data = {
+            'item': inventories[0].pk,
+            'quantity': 5,
+            'unit_price': '100.00'
+        }
+        url: str = reverse('orders-add-item', args=[order.pk])
+
+        # Assert that trying to add an invalid item fails
+        self.client.force_authenticate(user=customer.user)
+        response = self.client.post(url, {}, format='json')
+
+        self.assertEqual(order.orderitem_set.count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Assert that with the correct data, add works as expected
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order.orderitem_set.count(), 1)
+        self.assertEqual(response.data['quantity'], 5)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Assert that non-staff users cannot modify item prices
+        self.assertNotEqual(response.data['unit_price'], '100.00')
+
+        # Assert that multiple items can be added to an order
+        data['item'] = inventories[1].pk
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order.orderitem_set.count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Assert that a customer cannot alter the item list of another
+        # customer's order
+        self.client.force_authenticate(user=customer2.user)
+        data['item'] = inventories[2].pk
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order.orderitem_set.count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # Assert that staff members can alter the item list of any customer's
+        # order
+        self.client.force_authenticate(user=admin)
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order.orderitem_set.count(), 3)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Assert that staff users can modify item prices
+        self.assertEqual(response.data['unit_price'], '100.00')
+
+        # Assert that adding an item that is already on an order's item list
+        # fails
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order.orderitem_set.count(), 3)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertDictEqual(
+            response.data,
+            {
+                'item': ['This item already exists in this order.']
+            }
+        )
+
+        # Assert that adding items to pending orders is allowed
+        url: str = reverse('orders-add-item', args=[order3.pk])
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order3.orderitem_set.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Assert that adding items to non created nor pending orders fail
+        # APPROVED ORDERS
+        url: str = reverse('orders-add-item', args=[order1.pk])
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order1.orderitem_set.count(), 0)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+        # Assert that adding items to non created nor pending orders fail
+        # CANCELED ORDERS
+        url: str = reverse('orders-add-item', args=[order2.pk])
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order2.orderitem_set.count(), 0)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+        # Assert that adding items to non created nor pending orders fail
+        # REJECTED ORDERS
+        url: str = reverse('orders-add-item', args=[order4.pk])
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(order4.orderitem_set.count(), 0)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+    def test_list_orders(self) -> None:
+        """
+        Ensure that the **OrderViewSet.list** action works as expected.
+        """
+        # Test data
+        admin: User = AdminFactory.create()
+        customer: Customer = CustomerFactory.create()
+        customer2: Customer = CustomerFactory.create()
+        OrderFactory.create_batch(3, customer=customer)
+        OrderFactory.create_batch(5, customer=customer2)
+
+        # Request data
+        url: str = reverse('orders-list')
+
+        # Assert that when a customer is logged on, he/she cannot see other
+        # customer's orders
+        self.client.force_authenticate(user=customer.user)
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(len(response.data.get('orders')), 3)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.client.force_authenticate(user=customer2.user)
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(len(response.data.get('orders')), 5)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Assert that when an employee is logged on, he/she can see all the
+        # customers' orders
+        self.client.force_authenticate(user=admin)
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(len(response.data.get('orders')), 8)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_order(self) -> None:
+        """
+        Ensure that the **OrderViewSet.update** action works as expected.
+        """
+        # Test data
+        admin: User = AdminFactory.create()
+        customer: Customer = CustomerFactory.create()
+        order: Order = OrderFactory.create(customer=customer)
+
+        # Request data
+        post_data: Dict[str, Any] = OrderSerializer(order).data
+        post_data['state'] = Order.OrderState.get_value('pending')
+        patch_data = {
+            'state': Order.OrderState.get_value('pending')
+        }
+        url: str = reverse('orders-detail', args=[order.pk])
+
+        # Assert that customers cannot not update existing orders directly
+        self.client.force_authenticate(user=customer.user)
+        response = self.client.post(
+            url,
+            post_data,
+            format='json'
+        )  # POST request
+        self.assertNotEqual(
+            order.state,
+            Order.OrderState.get_value('pending')
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+        response = self.client.patch(
+            url,
+            patch_data,
+            format='json'
+        )  # PATCH request
+        self.assertNotEqual(
+            order.state,
+            Order.OrderState.get_value('pending')
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Assert that even admins cannot not update existing orders directly
+        self.client.force_authenticate(user=admin)
+        response = self.client.post(
+            url,
+            post_data,
+            format='json'
+        )  # POST request
+        self.assertNotEqual(
+            order.state,
+            Order.OrderState.get_value('pending')
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+        response = self.client.patch(
+            url,
+            patch_data,
+            format='json'
+        )  # PATCH request
+        self.assertNotEqual(
+            order.state,
+            Order.OrderState.get_value('pending')
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
